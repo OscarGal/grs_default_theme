3075e8a322cfa4c4a26bb535b91fdd4d746d89b7
Oscar Galvez <rgalvez@grs-electronics.com>
2020-03-24 13:41:14 -0600

    First Commit

---
+++ __manifest__.py
@@ -0,0 +1,29 @@
+{
+    # Theme information
+    'name': "Grs Default Theme",
+    'description': """
+    """,
+    'category': 'Theme',
+    'version': '1.0',
+    'depends': ['website'],
+
+    # templates
+    'data': [
+        'views/layout.xml',
+        'views/assets.xml',
+        'views/grs-home.xml',
+        # 'views/snippets.xml',
+    ],
+
+    # demo pages
+    'demo': [
+        'demo/pages.xml',
+    ],
+
+    # Your information
+    'author': "GRS Electronics",
+    'website': "http://grselectrodomesticos.com",
+    'images': [
+        'static/poster.png',
+    ],
+}
